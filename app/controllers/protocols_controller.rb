d5ed1f36007d996b870dbd77f3807cef2a311c94
Knut Neksa <knut.neksa@gmail.com>
2014-09-24 23:51:37 +0200

    Created functionality for adding new protocols.

--- app/controllers/protocols_controller.rb
+++ app/controllers/protocols_controller.rb
@@ -25,23 +25,31 @@ class ProtocolsController < ApplicationController
   # POST /protocols
   # POST /protocols.json
   def create
-    debtors = params.require(:debtors)
-    debtors.each do
-      @protocol = Protocol.new protocol_params
-      @protocol.buyer = current_user
+    @protocol = Protocol.new
+    @users = User.all
 
-      respond_to do |format|
-        if @protocol.save
-          format.html { redirect_to @protocol, notice: 'Protocol was successfully created.' }
-        else
-          format.html { render :new }
-        end
+    @protocol = Protocol.new protocol_params
+    @protocol.valid?
+    if not params.has_key? :debtors or (params[:debtors].one? and params[:debtors].values.first == current_user.id.to_s)
+      @protocol.errors[:debtors] = 'You must set at least one debtor other than yourself.'
+    end
+
+    if @protocol.errors.empty?
+      debtors = params.require(:debtors)
+      create_protocol_for_each_debtor debtors, protocol_params
+    end
+
+    respond_to do |format|
+      if @protocol.errors.empty?
+        format.html { redirect_to new_protocol_path, notice: 'Protocols were successfully created.' }
+      else
+        format.html { render :new }
       end
     end
   end
 
-  # PATCH/PUT /protocols/1
-  # PATCH/PUT /protocols/1.json
+# PATCH/PUT /protocols/1
+# PATCH/PUT /protocols/1.json
   def update
     respond_to do |format|
       if @protocol.update(protocol_params)
@@ -52,8 +60,8 @@ class ProtocolsController < ApplicationController
     end
   end
 
-  # DELETE /protocols/1
-  # DELETE /protocols/1.json
+# DELETE /protocols/1
+# DELETE /protocols/1.json
   def destroy
     @protocol.destroy
     respond_to do |format|
@@ -62,13 +70,33 @@ class ProtocolsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
+# Use callbacks to share common setup or constraints between actions.
   def set_protocol
     @protocol = Protocol.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
+# Never trust parameters from the scary internet, only allow the white list through.
   def protocol_params
     params.require(:protocol).permit(:ware, :price, :amount_owed)
+  end
+
+  def create_protocol_for_each_debtor(debtors, protocol_params)
+    if valid_debtors? debtors
+      debtors.each_key do |debtor_key|
+        protocol = Protocol.new protocol_params
+        protocol.buyer = current_user
+        protocol.debtor_id = debtors[debtor_key]
+        protocol.amount_owed = protocol_params[:price].to_f / debtors.length
+        protocol.save
+      end
+    end
+  end
+
+  def valid_debtors? (debtors)
+    debtors.each_value do |id|
+      unless User.exists? id
+        return false
+      end
+    end
   end
 end
--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,7 +1,11 @@
 class Protocol < ActiveRecord::Base
+  validates :ware, length: { minimum: 3 }
+  validates :price, numericality: { greater_than: 0 }
+  validates :amount_owed, numericality: { greater_than_or_equal_to: 0 }
+
   monetize :price_Øres
   monetize :amount_owed_Øres
 
-  belongs_to :buyer,   class_name: 'User'
-  belongs_to :debtor,  class_name: 'User'
+  belongs_to :buyer, class_name: 'User'
+  belongs_to :debtor, class_name: 'User'
 end
